e1099f91b8e7b8d45ed2d1b4a6d879fc9afee91e
Diego <d_diazmendoza@hotmail.com>
2020-07-06 17:48:18 -0500

    1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwapiClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
